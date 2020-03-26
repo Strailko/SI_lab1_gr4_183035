1d97682d94569d9dd79daa6287d2a0a8cfbdf774
Your Name <you@example.com>
2020-03-26 15:41:27 +0100

    point class finished

--- Point.java
+++ Point.java
@@ -6,14 +6,66 @@ class Point {
 	String boja;
 	//TODO constructor
 
+	public Point(String id, double x, double y, String boja) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.boja = boja;
+	}
+
 	//TODO setters and getters
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public String getBoja() {
+		return boja;
+	}
+
+	public void setBoja(String boja) {
+		this.boja = boja;
+	}
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+		if(xDirection=="L"){
+			x--;
+		}
+		else {
+			x++;
+		}
+		if(yDirection=="U"){
+			y++;
+		}
+		else {
+			y--;
+		}
 	}
 
 	public void draw () {
 		//TODO
+		System.out.println("("this.x+","+this.y")")
 	}
 
 
